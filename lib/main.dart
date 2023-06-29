65cf6d11a38b54ba6cf332c04be536f22e17957a
vikrant-bioenable <vikrant@bioenabletech.com>
2023-06-29 12:29:53 +0530

    feature - defined app routes

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:pcmc_staff/screens/alerts.dart';
+import 'package:pcmc_staff/screens/attendance_dashboard.dart';
+import 'package:pcmc_staff/screens/attendance_logs.dart';
+import 'package:pcmc_staff/screens/forgot_password.dart';
+import 'package:pcmc_staff/screens/home.dart';
+import 'package:pcmc_staff/screens/monthly_timecard.dart';
+import 'package:pcmc_staff/screens/profile.dart';
+import 'package:pcmc_staff/screens/route_map.dart';
+import 'package:pcmc_staff/screens/sign_in.dart';
+import 'package:pcmc_staff/screens/task_list.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,31 +22,24 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'PCMC Staff',
+      routes: {
+        '/sign_in': (context) => const SignIn(),
+        '/forgot_password': (context) => const ForgotPassword(),
+        '/home': (context) => const Home(),
+        '/attendance_dashboard': (context) => const AttendanceDashboard(),
+        '/attendance_logs': (context) => const AttendanceLogs(),
+        '/time_card': (context) => const MonthlyTimeCard(),
+        '/route_map': (context) => const RouteMap(),
+        '/profile': (context) =>
+            const Profile(), // from profile when user clicks on edit/update - he/she can update profile with profileUpdate Alert Dialog
+        '/tasks_list': (context) => const TaskList(),
+        '/alerts': (context) => const Alerts(),
+      },
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'PCMC Staff'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
+      home: const SignIn(),
     );
   }
 }
