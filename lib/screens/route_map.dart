c33aea5fb6f85afb2e0692c93b177e05ccd1ce8e
BioEnable-bit <sahil@bioenabletech.com>
2023-07-03 18:38:10 +0530

    Features: Map load Speed Improved And changes In route map,live Map,bin dashboard screen changed

--- lib/screens/route_map.dart
+++ lib/screens/route_map.dart
@@ -1,39 +1,52 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
-import 'home.dart';
-
 class RouteMap extends StatefulWidget {
-  const RouteMap({super.key});
-
+  // final String _appTitle='Bin DashBoard';
+  // final String _connectionString='https://pcmc.geodirect.in/gps/cctv/bin-status-map.php';
+  //
   @override
-  State<RouteMap> createState() => _RouteMapState();
+  _RouteMapState createState() =>
+      _RouteMapState();
 }
 
 class _RouteMapState extends State<RouteMap> {
+  final String _appTitle='Bin DashBoard';
+  final String _connectionString='https://pcmc.geodirect.in/gps/route-pcmc1.php?date=2023-06-30&carrier=866330053191635&orgid=922677&vtype=1';
+
+  int _stackToView = 1;
+  final _key = GlobalKey();
+  _RouteMapState();
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        leading: IconButton(
-          icon: const Icon(Icons.arrow_back),
-          onPressed: () => Navigator.pushReplacement(context,
-              MaterialPageRoute(builder: (context) {
-            return const Home();
-            // Navigator.pop(context);
-          })),
-        ),
-        title: const Text("Route Map"),
-      ),
-      body: SizedBox(
-        child: SizedBox(
-          width: MediaQuery.of(context).size.width,
-          height: MediaQuery.of(context).size.height,
-          child: const WebView(
-            initialUrl:
-                "https://pcmc.geodirect.in/gps/route-pcmc1.php?date=2023-06-30&carrier=866330053191635&orgid=922677&vtype=1",
-            javascriptMode: JavascriptMode.unrestricted,
+    return CupertinoPageScaffold(
+      navigationBar: CupertinoNavigationBar(middle: Text('Route Map',style: TextStyle(fontSize: 30),)),
+      child: Container(
+        child: SafeArea(
+          child: IndexedStack(
+            index: _stackToView,
+            children: <Widget>[
+              WebView(
+                key: _key,
+                javascriptMode: JavascriptMode.unrestricted,
+                initialUrl: this._connectionString,
+                onPageStarted: (value) => setState(() {
+                  _stackToView = 1;
+                }),
+                onPageFinished: (value) => setState(() {
+                  _stackToView = 0;
+                }),
+              ),
+              Container(
+                color: Color.fromRGBO(250, 250, 250, 1),
+                child: Center(
+                  child: CircularProgressIndicator(color: Colors.deepPurple),
+                ),
+              )
+            ],
           ),
+          top: true,
         ),
       ),
     );
